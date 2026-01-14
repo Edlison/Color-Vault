b021aa4866a1a87808a17f12de1ea7eff4dbcf3b
Edlison <bolin.edlison.shen@gmail.com>
2026-01-14 06:04:07 -0500

    update baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => ({
   // GitHub Pages (Project Pages) 默认路径是 /<repo>/
   // 如果你的仓库名不是 Color-Vault，把下面改成 '/你的仓库名/'
-  base: mode === 'production' ? '/color/' : '/',
+  base: mode === 'production' ? './' : '/',
   plugins: [react(), tailwindcss()],
 }))
